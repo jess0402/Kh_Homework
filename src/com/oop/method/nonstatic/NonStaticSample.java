40509d8aa2eee7d5620c9c9b8d9636ddb1ae380d
jess0402 <jyjmjs2@naver.com>
2022-02-09 17:00:31 +0900

    220209 Homework

--- src/com/oop/method/nonstatic/NonStaticSample.java
+++ src/com/oop/method/nonstatic/NonStaticSample.java
@@ -61,6 +61,10 @@ public class NonStaticSample {
 		return n;	
 	}
 	
+	//4. 반환값 있고 매개변수 있는 메소드
+	//실행 요청시 문자열과 시작인덱스, 끝인덱스를 전달받아 해당 인덱스 범위의 문자열을
+	//추출하여 리턴하는 메소드. 단 문자열은 반드시 값이 있어야함. 없으면 null 리턴처리
+	//메소드명 : mySubstring
 	public String mySubstring(String s, int startnum, int finishnum) {
 		
 		if(s==null)
